b57be72f5da657d8f0f34a75bed67db339dd8bd2
Tim Jochen Kicker <tim@kicker.dev>
2026-04-26 13:03:32 +0200

    os-sdk: add shell.presence, timeline, spatial, annotations   modules

--- os-sdk/src/lib.rs
+++ os-sdk/src/lib.rs
@@ -1,7 +1,11 @@
+pub mod annotations;
 pub mod config;
 pub mod event;
 pub mod graph;
 pub mod mock;
+pub mod presence;
+pub mod spatial;
+pub mod timeline;
 
 pub(crate) mod proto {
     #![allow(dead_code)]
@@ -9,6 +13,12 @@ pub(crate) mod proto {
     include!(concat!(env!("OUT_DIR"), "/lunaris.eventbus.rs"));
 }
 
+pub use annotations::{
+    AnnotationLookup, AnnotationRecord, AnnotationSetParams, AnnotationTarget, Annotations,
+};
 pub use event::{EmitError, EventEmitter, UnixEventEmitter};
 pub use graph::{GraphClient, QueryError, UnixGraphClient};
+pub use presence::{AutoClear, Presence, PresenceParams};
+pub use spatial::{GeometryHint, OutputHint, Spatial, SpatialHint};
+pub use timeline::{Timeline, TimelineParams};
 pub mod shell_types;
